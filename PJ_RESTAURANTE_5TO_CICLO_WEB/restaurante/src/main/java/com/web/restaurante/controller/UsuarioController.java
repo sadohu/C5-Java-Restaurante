bd7943d121105911383a5304cc957d64106a8742
LuisRuiz4200 <lr27072001@gmail.com>
2023-07-13 14:19:52 -0500

    Update UsuarioController.java

--- PJ_RESTAURANTE_5TO_CICLO_WEB/restaurante/src/main/java/com/web/restaurante/controller/UsuarioController.java
+++ PJ_RESTAURANTE_5TO_CICLO_WEB/restaurante/src/main/java/com/web/restaurante/controller/UsuarioController.java
@@ -1,8 +1,14 @@
 package com.web.restaurante.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UsuarioController {
 
+	@GetMapping("/")
+	private String loginUsuario () {
+		return "login";
+	}
+	
 }
